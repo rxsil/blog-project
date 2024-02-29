a64524862de193ca568f2f423e0bed75d6e28763
rxsil <155285668+rxsil@users.noreply.github.com>
2024-02-29 08:14:19 +0000

    -3- Creating Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import {images} from "../constants"
 
 const Header = () => {
   return (
    <section>
    <header>
     <div>
-      <img src="" alt="logo"/>
+      <img src={images.Logo} alt="logo"/>
     </div>
     
     <div>
     <ul>
-    <li><a href="">Home</a></li>
-    <li><a href="">Article</a></li>
-    <li><a href="">Page</a></li>
-    <li><a href="">Pricing</a></li>
-    <li><a href="">Faq</a></li>
+    <li><a href="/">Home</a></li>
+    <li><a href="/">Article</a></li>
+    <li><a href="/">Page</a></li>
+    <li><a href="/">Pricing</a></li>
+    <li><a href="/">Faq</a></li>
 
       </ul>
      </div>
